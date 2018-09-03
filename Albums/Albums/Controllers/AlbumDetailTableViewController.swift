e0ed2c9c3e836a57adc3c6baa3899fd02bc50802
Lambda-School-Loaner-11 <lambda-school-loaner-11@Lambda-School-Loaner-11s-MacBook-Pro.local>
2018-09-03 12:36:51 -0700

    set up ALL storyboards

--- Albums/Albums/Controllers/AlbumDetailTableViewController.swift
+++ Albums/Albums/Controllers/AlbumDetailTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  AlbumsTableViewController.swift
+//  AlbumDetailTableViewController.swift
 //  Albums
 //
 //  Created by Lambda-School-Loaner-11 on 9/3/18.
@@ -8,23 +8,35 @@
 
 import UIKit
 
-class AlbumsTableViewController: UITableViewController {
+class AlbumDetailTableViewController: UITableViewController {
+    
+    @IBOutlet weak var albumName: UITextField!
+    
+    @IBOutlet weak var artistName: UITextField!
+    
+    @IBOutlet weak var genreName: UITextField!
+    
+    @IBOutlet weak var urlName: UITextField!
+    
+    @IBAction func save(_ sender: Any) {
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
+        
         return 0
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "detailCell", for: indexPath)
 
         return cell
     }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
        
     }
---
+++ Albums/Albums/Views/SongTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  SongTableViewCell.swift
+//  Albums
+//
+//  Created by Lambda-School-Loaner-11 on 9/3/18.
+//  Copyright © 2018 David Doswell. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var songTitle: UITextField!
+    
+    @IBOutlet weak var songDuration: UITextField!
+    
+    @IBAction func addSong(_ sender: Any) {
+    }
+    
+}
