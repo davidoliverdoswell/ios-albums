df085a31e9acf82ad03e654987c327d89a3c7670
Lambda-School-Loaner-11 <lambda-school-loaner-11@Lambda-School-Loaner-11s-MacBook-Pro.local>
2018-09-03 11:57:14 -0700

    set up storyboards

---
+++ Albums/Albums/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Albums
+//
+//  Created by Lambda-School-Loaner-11 on 9/3/18.
+//  Copyright © 2018 David Doswell. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        
+
+        return true
+    }
+
+}
